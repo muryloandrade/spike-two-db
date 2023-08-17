0b888f5b8ebf0a3ea5b3572dbd29c02d054cff21
Murylo Andrade Silva <muryloandrades@gmail.com>
2023-08-17 16:18:25 -0300

    get user data id

--- api/controllers/controllers.go
+++ api/controllers/controllers.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
@@ -15,6 +17,37 @@ func HelloWorld(w http.ResponseWriter, r *http.Request) {
 
 func GetDatabaseOne(w http.ResponseWriter, r *http.Request) {
 	rows, err := database.DBONE.QueryContext(r.Context(), "SELECT * FROM users")
+	if err != nil {
+		log.Fatal("Error executing query:", err)
+	}
+	defer rows.Close()
+
+	var users []entity.User // Assuming you have defined a User struct to represent the data
+
+	for rows.Next() {
+		var user entity.User
+
+		err := rows.Scan(&user.Id, &user.Nome, &user.Cargo) // Scan column values into struct fields
+		if err != nil {
+			log.Fatal("Error scanning row:", err)
+		}
+
+		users = append(users, user)
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Fatal("Error after iterating through rows:", err)
+	}
+
+	json.NewEncoder(w).Encode(users)
+}
+
+func GetDatabaseOneId(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	rows, err := database.DBONE.QueryContext(r.Context(), "SELECT * FROM users WHERE Id = $1", id)
 	if err != nil {
 		log.Fatal("Error executing query:", err)
 	}
--- api/routes/routes.go
+++ api/routes/routes.go
@@ -15,7 +15,8 @@ func HandleResquests() {
 	r := mux.NewRouter()
 	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.HelloWorld).Methods("GET")
-	r.HandleFunc("/data1", controllers.GetDatabaseOne).Methods("GET")
-	r.HandleFunc("/data2", controllers.GetDatabaseTwo).Methods("GET")
+	r.HandleFunc("/usersdata1", controllers.GetDatabaseOne).Methods("GET")
+	r.HandleFunc("/usersdata1/{id}", controllers.GetDatabaseOneId).Methods("GET")
+	r.HandleFunc("/personadata2", controllers.GetDatabaseTwo).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
